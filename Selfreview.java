430793e946518349bb8ef9215d6f246b998f2f1c
minhltitraining <137228447+minhltitraining@users.noreply.github.com>
2023-07-01 22:31:36 -0500

    Create Selfreview.java

---
+++ Selfreview.java
@@ -0,0 +1,17 @@
+/*
+1) Equal
+2) Not Equal
+3) Equal
+4) Static block written before main
+  Static block written after main
+  12
+  12
+5)Mrs.Vani
+  Mr.Nishit
+6) Compile error: java: non-static variable num cannot be referenced from a static context
+7) The area of the Square is 25
+
+
+
+
+*/
